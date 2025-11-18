1209a528f1a8200f1eac608274de8188257546e7
Somesh Chandimal <someshchandih@gmail.com>
2025-11-18 19:42:33 +0530

    fix: correct resource URL for printing receipts in printer service

--- src/services/printerService.ts
+++ src/services/printerService.ts
@@ -1,6 +1,6 @@
 import { InsertSalesPayload } from "@/types/sale-types";
 import http from "./httpService";
 
-const RESOURCE = "/print-receipt";
+const RESOURCE = "/print/receipt";
 
 export const sendToPrint = async (data: InsertSalesPayload) => http.post(`${RESOURCE}`, { salesData: data });
